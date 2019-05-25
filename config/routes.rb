e9abecc4624e6a5ef7e8ee4867fc17412af5af53
zarszz <zarszzntunx@rocketmail.com>
2019-05-25 10:40:44 +0700

    add several changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
-  post 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
 
